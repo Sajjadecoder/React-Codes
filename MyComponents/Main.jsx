d0a5dd092aa5d66057a935891bd7ebfac2285aab
Sajjadecoder <sajjadahmed102005@gmail.com>
2024-12-31 22:19:36 +0500

    Chef Claude dynamic ingredients adding

--- MyComponents/Main.jsx
+++ MyComponents/Main.jsx
@@ -1,13 +1,42 @@
 import './main.css'
+import { useState } from 'react';
 export default function Main(){
+    // let ingredients= ["Oranges","Potatoes","Tomatoes"]
+    // let mapped = ingredients.map(x=>{
+    //     return <li key={x}>{x}</li>
+    // })
+    // function handleFormSubmit(event){
+      
+    //   event.preventDefault();//disallow page to be refreshed
+    // }
+
+
+
+
+
+
+    const [ingredients,addIngredient] = useState([])
+    const mapped = ingredients.map(x=>{ return <li key={x}>{x}</li>}) 
+    function handleFormSubmit(event){
+      event.preventDefault()
+      const formData = new FormData(event.currentTarget);
+      const newIngredient = formData.get('inputIngredient');
+      
+      if (newIngredient) {
+        
+        addIngredient(prevIngredients=>[...prevIngredients,newIngredient])
+      }
+    }
     return(
       <>
-        <main>
-            <form className='add-ingredient-form'>
+        <main >
 
-            <input type="text" size={50} placeholder="Enter smth" />
+            <form className='add-ingredient-form' onSubmit={handleFormSubmit}>
+
+            <input type="text" size={50} placeholder="Enter smth" name='inputIngredient' />
             <button>+ Add Ingredient</button>
             </form>
+            {mapped}
         </main>
 
       </>
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import Header from '../MyComponents/Header'
 import Main from '../MyComponents/Main'
 import './index.css'
-
+import { useState } from 'react'  
 
 function App() {
-
+  const [arr,addNewValInArr] = useState([])
+  const mappedArr = arr.map(x=> 
+    <h3>{x}</h3>
+  )
+  function pushVal(params) {
+    addNewValInArr(prevArr=>[...prevArr,"Test"])
+  }
   return (
     <>
+
       <Header />
       <Main />
+      <button onClick={pushVal}>Add Item</button>
+      {mappedArr}
     </>
   )
 }
