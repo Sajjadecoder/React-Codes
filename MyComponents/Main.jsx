00a1648b23d4c6a2fb77133c050f8f3dbbc7c43f
Sajjadecoder <sajjadahmed102005@gmail.com>
2025-01-01 22:38:48 +0500

    Conditional rendering

--- MyComponents/Main.jsx
+++ MyComponents/Main.jsx
@@ -1,4 +1,5 @@
 import './main.css'
+import FinalContent from './FinalContent';
 import { useState } from 'react';
 export default function Main(){
     // let ingredients= ["Oranges","Potatoes","Tomatoes"]
@@ -10,22 +11,22 @@ export default function Main(){
     //   event.preventDefault();//disallow page to be refreshed
     // }
 
-
-
-
-
-
     const [ingredients,addIngredient] = useState([])
     const mapped = ingredients.map(x=>{ return <li key={x}>{x}</li>}) 
+    const [isEmpty,setIsEmpty]= useState(true)
+    const [btnClicked,setBtn]= useState(false)
     function handleFormSubmit(event){
       event.preventDefault()
       const formData = new FormData(event.currentTarget);
       const newIngredient = formData.get('inputIngredient');
       
       if (newIngredient) {
-        
+        setIsEmpty(false)
         addIngredient(prevIngredients=>[...prevIngredients,newIngredient])
       }
+    }
+    function renderFinalContent(){
+      setBtn(true)
     }
     return(
       <>
@@ -36,8 +37,23 @@ export default function Main(){
             <input type="text" size={50} placeholder="Enter smth" name='inputIngredient' />
             <button>+ Add Ingredient</button>
             </form>
-            {mapped}
+            {!isEmpty && (
+              <h3>Ingredients on hand:</h3>
+            )}
+            { mapped}
         </main>
+
+        {!isEmpty && <footer className='get-recipe'>
+          <section>
+
+          <h4>Ready for a Recipe?</h4>
+          <p>Generate a recipe from your list of ingredients</p>
+          </section>
+          <button className='get-recipe-btn' onClick={renderFinalContent}>Get a recipe</button>
+        </footer>
+        }
+        {btnClicked && <FinalContent/>}
+
 
       </>
     );
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from '../MyComponents/Header'
 import Main from '../MyComponents/Main'
+import FinalContent from '../MyComponents/FinalContent'
 import './index.css'
 import { useState } from 'react'
 
@@ -12,62 +13,100 @@ function App() {
   //   addNewValInArr(prevArr=>[...prevArr,"Test"])
   // }
   //imp: if buttons are placed inside a form,they work like input type="submit"
-  const [userEmail, setEmail] = useState(null)
-  const [userPass, setPassword] = useState(null)
-  const [userGender, setGender] = useState(null)
-  function printUserData(event) {
-    event.preventDefault();
-    const newData = event.currentTarget
-    const formData = new FormData(newData)
-    const email = formData.get('Email')
-    const password = formData.get('password')
-    const gender = formData.get('gender')
-    // console.log(email)
-    // console.log(password)
-    // console.log(gender)
-    setGender(gender)
-    setPassword(password)
-    setEmail(email)
-  }
+
+
+  //exercise: 
+  // const [unreadMsgs,setUnread] = useState(10)
+  {/* {unreadMsgs==0 && (<h1>There are no unread msgs</h1>)}
+  {unreadMsgs>0 && (<h1>There are {unreadMsgs} unread msgs</h1>)} */}
   return (
     <>
-      <h1>Signup Form</h1>
-      <form method='post' onSubmit={printUserData}>
-        <label htmlFor="email">Email: </label>
-        <input type="email" name='Email' placeholder='Enter Email' id='email' />
-        <label htmlFor="password">Password: </label>
-        <input type="password" name='password' placeholder='Enter Password' id='password' />
-        <label htmlFor="choice1">
-          <input type="radio" id='choice1' name='gender' value={'Male'} />
-          Male
-        </label>
-        <label htmlFor="choice2">
-          <input type="radio" id='choice2' name='gender' value={'Female'} />
-          Female
-        </label>
-        <label htmlFor="choice3">
-          <input type="radio" id='choice3' name='gender' value={'Custom'} />
-          Custom
-        </label>
-        {/* name should be same for each radio button(in this case the name is gender) */}
-        {/* value is required when submitting the form to access what is being selected by the user */}
 
-        <button>Click to Submit</button>
-
-        {userEmail && (
-          <div>
-
-            Your Email: {userEmail}
-            <br />
-            Your Password: {userPass}
-            <br />
-            Your Gender: {userGender}
-          </div>
-        )
-        }
-      </form>
+      <Header/>
+      <Main/>
+      
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  {
+    
+    /* <h1>Signup Form</h1>
+  <form method='post' onSubmit={printUserData}>
+    <label htmlFor="email">Email: </label>
+    <input type="email" name='Email' placeholder='Enter Email' id='email' />
+    <label htmlFor="password">Password: </label>
+    <input type="password" name='password' placeholder='Enter Password' id='password' />
+    <label htmlFor="choice1">
+      <input type="radio" id='choice1' name='gender' value={'Male'} />
+      Male
+    </label>
+    <label htmlFor="choice2">
+      <input type="radio" id='choice2' name='gender' value={'Female'} />
+      Female
+    </label>
+    <label htmlFor="choice3">
+      <input type="radio" id='choice3' name='gender' value={'Custom'} />
+      Custom
+    </label>
+    {/* name should be same for each radio button(in this case the name is gender) */}
+    {/* value is required when submitting the form to access what is being selected by the user */}
+
+  //   <button>Click to Submit</button>
+
+  //   {userEmail && (
+  //     <div>
+
+  //       Your Email: {userEmail}
+  //       <br />
+  //       Your Password: {userPass}
+  //       <br />
+  //       Your Gender: {userGender}
+  //     </div>
+  //   )
+  //   }
+  // </form> */}  
